b9e7023456452fda8e0db243e3d86f4d0c31d786
Curtis Burkhalter <curtisburkhalter@gmail.com>
2018-07-03 06:12:39 -0400

    Read in data
    This file uses the package 'googlesheets' to access data I got from the internet for the English Premier League. Data from footstats.co.uk

---
+++ Scripts/ReadFiles_FromGoogleSheets.R
@@ -0,0 +1,27 @@
+suppressMessages(library(tidyverse))
+suppressMessages(library(here))
+suppressMessages(library(googlesheets))
+
+#get the EPL_Goals datasets from Google sheets
+goals1718 <- gs_title("EPL_Goals1718")
+goals1617 <- gs_title("EPL_Goals1617")
+goals1516 <- gs_title("EPL_Goals1516")
+goals1415 <- gs_title("EPL_Goals1415")
+goals1314 <- gs_title("EPL_Goals1314")
+goals1213 <- gs_title("EPL_Goals1213")
+goals1112 <- gs_title("EPL_Goals1112")
+goals1011 <- gs_title("EPL_Goals1011")
+goals0910 <- gs_title("EPL_Goals0910")
+goals0809 <- gs_title("EPL_Goals0809")
+
+#read the sheets into R
+goals1718 <- gs_read(ss=goals1718)
+goals1617 <- gs_read(ss=goals1617)
+goals1516 <- gs_read(ss=goals1516)
+goals1415 <- gs_read(ss=goals1415)
+goals1314 <- gs_read(ss=goals1314)
+goals1213 <- gs_read(ss=goals1213)
+goals1112 <- gs_read(ss=goals1112)
+goals1011 <- gs_read(ss=goals1011)
+goals0910 <- gs_read(ss=goals0910)
+goals0809 <- gs_read(ss=goals0809)
