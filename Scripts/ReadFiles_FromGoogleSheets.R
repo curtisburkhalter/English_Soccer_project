6a914cc608db5593cc54fdf9aadc4408f73e35ba
Curtis Burkhalter <curtisburkhalter@gmail.com>
2018-07-06 21:36:34 -0400

    Combined annual dataframes
    This commit contains a few lines of code that combined the annual dataframes into a single dataframe

--- Scripts/ReadFiles_FromGoogleSheets.R
+++ Scripts/ReadFiles_FromGoogleSheets.R
@@ -25,3 +25,13 @@ goals1112 <- gs_read(ss=goals1112)
 goals1011 <- gs_read(ss=goals1011)
 goals0910 <- gs_read(ss=goals0910)
 goals0809 <- gs_read(ss=goals0809)
+
+#rbind together all the seasons
+combined <- rbind(goals1718,goals1617,goals1516,goals1415,goals1314,goals1213,goals1112,goals1011,goals0910,goals0809)
+
+#add season index column to combined
+seasons <- c("1718","1617","1516","1415","1314","1213","1112","1011","0910","0809")
+
+seasons_rep <- rep(seasons, each = 20)
+
+combined <- cbind(combined,seasons_rep)
