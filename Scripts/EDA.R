ef316397611ccf1eafaa0dd44ceb819fc1e69e2c
Curtis Burkhalter <curtisburkhalter@gmail.com>
2018-07-06 21:38:20 -0400

    Breakout annual goals for/against
    This initial phase of EDA looks at the total number of goals scored and total number allowed; includes breakouts for top 6 finishers and bottom 3 finishers

---
+++ Scripts/EDA.R
@@ -0,0 +1,107 @@
+#read in required libraries
+suppressMessages(library(tidyverse))
+suppressMessages(library(here))
+suppressMessages(library(googlesheets))
+suppressMessages(library(gridExtra))
+
+#read in files using source file
+source(here("English_Soccer_project","Scripts","ReadFiles_FromGoogleSheets.R"))
+
+#all individual files read in from the source file were combined into a single dataframe 'combined'
+
+#insert a "/" character into the seasons_rep column
+combined$seasons_rep <- paste(substr(combined$seasons_rep,1,2),"/",substr(combined$seasons_rep,3,4),sep="")
+
+#plot number of instances of each team was in the Premier League from 2008 - 2018
+combined %>%
+group_by(Team) %>%
+summarize(Total = n()) %>%
+arrange(., desc(Total)) %>%
+ggplot(data = ., aes(x = reorder(Team,-Total), y = Total, fill = Team)) +
+  geom_col() +
+  coord_flip() +
+  guides(fill = FALSE) +
+  xlab("Team") +
+  scale_y_continuous(breaks = seq(0,10,1)) +
+  ylab("Total seasons in Premier League from 2008 - 2018")
+
+#look at distribution of goals for and goals against for each year
+For_overall <- ggplot(combined, aes(x = seasons_rep, y = FTotal, fill = seasons_rep)) +
+               geom_boxplot() +
+               geom_smooth(method = "loess", aes(group = 1), se = FALSE, color = "black", lty = 2) +
+               ylab("Number of goals") +
+               xlab("Season") +
+               ylim(20,110) +
+               labs(title = "Annual distribution of total team goals scored") +
+               guides(fill = FALSE)
+
+Against_overall <- ggplot(combined, aes(x = seasons_rep, y = ATotal, fill = seasons_rep)) +
+                   geom_boxplot() +
+                   geom_smooth(method = "loess", aes(group = 1), se = FALSE, color = "black", lty = 2) +
+                   ylab("Number of goals") +
+                   xlab("Season") +
+                   ylim(20,110) +
+                   labs(title = "Annual distribution of total team goals allowed") +
+                   guides(fill = FALSE)
+
+#plot annual total goals for and total goals against across all teams
+grid.arrange(For_overall,Against_overall, nrow = 1, ncol =2)
+
+#look at goals scored by top 6 teams (i.e. those who qualify for either Champions League or Europa League) for changes
+#through time; question being are the top 6 teams scoring more/less goals through time
+For_top6 <- combined %>%
+filter(Position <= 6) %>%
+ggplot(data = ., aes(x = seasons_rep, y = FTotal, fill = seasons_rep)) +
+  geom_boxplot() +
+  geom_smooth(method = "loess", aes(group = 1), se = FALSE, color = "black", lty = 2) +
+  ylab("Number of goals") +
+  xlab("Season") +
+  ylim(20,110) +
+  labs(title = "Annual distribution of total team goals scored", subtitle = "Includes only the top 6 teams per year") +
+  guides(fill = FALSE)
+
+
+#look at goals scored by bottom 3 teams (i.e. those who are relegated at end of season) for changes
+#through time; question being are the bottom 3 teams scoring more/less goals through time
+For_bottom3 <- combined %>%
+  filter(Position >= 18) %>%
+  ggplot(data = ., aes(x = seasons_rep, y = FTotal, fill = seasons_rep)) +
+  geom_boxplot() +
+  geom_smooth(method = "loess", aes(group = 1), se = FALSE, color = "black", lty = 2) +
+  ylab("Number of goals") +
+  xlab("Season") +
+  ylim(20,110) +
+  labs(title = "Annual distribution of total team goals scored", subtitle = "Includes only the bottom 3 teams per year") +
+  guides(fill = FALSE)
+
+#plot annual total goals for broken out by top 6 and bottom 3 teams
+grid.arrange(For_top6,For_bottom3, nrow = 1, ncol = 2)
+
+#look at goals allowed by top 6 teams (i.e. those who qualify for either Champions League or Europa League) for changes
+#through time; question being are the top 6 teams scoring more/less goals through time
+Against_top6 <- combined %>%
+  filter(Position <= 6) %>%
+  ggplot(data = ., aes(x = seasons_rep, y = ATotal, fill = seasons_rep)) +
+  geom_boxplot() +
+  geom_smooth(method = "loess", aes(group = 1), se = FALSE, color = "black", lty = 2) +
+  ylab("Number of goals") +
+  xlab("Season") +
+  ylim(20,90) +
+  labs(title = "Annual distribution of total team goals allowed", subtitle = "Includes only the top 6 teams per year") +
+  guides(fill = FALSE)
+
+#look at goals allowed by bottom 3 teams (i.e. those who are relegated at end of season) for changes
+#through time; question being are the bottom 3 teams scoring more/less goals through time
+Against_bottom3 <- combined %>%
+  filter(Position >= 18) %>%
+  ggplot(data = ., aes(x = seasons_rep, y = ATotal, fill = seasons_rep)) +
+  geom_boxplot() +
+  geom_smooth(method = "loess", aes(group = 1), se = FALSE, color = "black", lty = 2) +
+  ylab("Number of goals") +
+  xlab("Season") + 
+  ylim(20,90) +
+  labs(title = "Annual distribution of total team goals allowed", subtitle = "Includes only the bottom 3 teams per year") +
+  guides(fill = FALSE)
+
+#plot annual total goals against broken out by top 6 and bottom 3 teams
+grid.arrange(Against_top6,Against_bottom3, nrow = 1, ncol = 2)
